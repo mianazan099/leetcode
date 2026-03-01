3a35c645f0c7051b99bd8c4334c5fd38aefc7329
mianazan099 <103571768+mianazan099@users.noreply.github.com>
2026-03-02 00:55:49 +0500

    Time: 4 ms (52.54%) | Memory: 55.5 MB (39.21%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,9 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const numMap = new Map();
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j <= nums.length; j++) {
-            if (nums[i] + nums[j] === target) return [i, j];
-        }
+        let diff = target - nums[i];
+        if (numMap.has(diff)) return [numMap.get(diff), i]; else numMap.set(nums[i], i);
     }
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i+1; j <= nums.length; j++) {
+    //         if (nums[i] + nums[j] === target) return [i, j];
+    //     }
+    // }
 };
