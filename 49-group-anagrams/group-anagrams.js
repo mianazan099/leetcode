1ca38d4a870c4cf75c8a2e50c2d3ca6c7f1fa9ec
mianazan099 <103571768+mianazan099@users.noreply.github.com>
2026-03-04 08:35:48 +0500

    Time: 21 ms (94.33%) | Memory: 67.3 MB (69.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,12 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-        const nMap = new Map();
+        const map = new Map();
         strs.forEach((e)=>{
             let word = e.split('').sort().join('');
-            if (nMap.has(word)) nMap.set(word, [nMap.get(word)[0]+1, [...nMap.get(word)[1], e]]); else nMap.set(word, [1, [e]])
+            if (map.has(word)) {
+                map.set(word, [...map.get(word), e])
+            } else {
+                map.set(word, [e])
+            }
         })
-        const nArr = []
-        nMap.forEach((e)=>nArr.push(e[1]))
-        return nArr
+        return [...map.values()]
 };
